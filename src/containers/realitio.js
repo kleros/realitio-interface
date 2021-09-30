a298b17105d51a8169b007cd34cacec259db7729
Ferit Tunçer <ferit@cryptolab.net>
2021-09-30 22:16:37 +0300

    refactor(realitio): styling and iframe resizer

--- src/containers/realitio.js
+++ src/containers/realitio.js
@@ -1,92 +1,69 @@
 import React, { Component } from "react";
-import styled from "styled-components/macro";
 import Web3 from "web3";
 
 import RealitioContract from "../assets/contracts/realitio.json";
 import RealitioProxyContract from "../assets/contracts/realitio-proxy.json";
-import RealitioLogo from "../assets/images/realitio_logo.png";
+import RealityLogo from "../assets/images/realitio_logo.png";
 
-const web3 = new Web3(window.web3 ? window.web3.currentProvider || "https://mainnet.infura.io/v3/668b3268d5b241b5bab5c6cb886e4c61" : "https://mainnet.infura.io/v3/668b3268d5b241b5bab5c6cb886e4c61");
+const web3 = new Web3(
+  window.web3
+    ? window.web3.currentProvider ||
+      "https://mainnet.infura.io/v3/668b3268d5b241b5bab5c6cb886e4c61"
+    : "https://mainnet.infura.io/v3/668b3268d5b241b5bab5c6cb886e4c61"
+);
 // Global CSS
-const QuestionText = styled.div`
-  margin-bottom: 15px;
-  font-family: "Roboto", sans-serif;
-  font-size: 20px;
-  font-variant: tabular-nums;
-  line-height: 1.5;
-  color: rgba(0, 0, 0, 0.65);
-  background-color: #fff;
-`;
-const QuestionLink = styled.a`
-  margin-top: 0px;
-  font-family: "Roboto", sans-serif;
-  font-size: 14px;
-  font-variant: tabular-nums;
-  line-height: 1.5;
-  color: rgba(0, 0, 0, 0.65);
-  background-color: #fff;
-  color: #2093ff;
-`;
-
-const Header = styled.div`
-  height: 80px;
-  margin-bottom: 25px;
-  overflow-x: hidden;
-  img {
-    height: 50%;
-    margin: 16px;
-  }
-`;
-
-const SquareLeft = styled.div`
-  position: absolute;
-  left: -35px;
-  top: -190px;
-  width: 100%;
-  height: 180px;
-  background-color: #56bbe8;
-  transform: rotate(-10deg);
-  z-index: -1;
-`;
-
-const SquareRight = styled.div`
-  position: absolute;
-  right: -40px;
-  top: -160px;
-  width: 100%;
-  height: 180px;
-  background-color: #cae940;
-  transform: rotate(4deg);
-  z-index: -2;
-`;
 
 class RealitioDisplayInterface extends Component {
   state = { question: null };
 
   async componentDidMount() {
     if (window.location.search[0] !== "?") return;
-    const message = JSON.parse(window.location.search.substring(1).replace(/%22/g, '"').replace(/%7B/g, "{").replace(/%3A/g, ":").replace(/%2C/g, ",").replace(/%7D/g, "}"));
+    const message = JSON.parse(
+      window.location.search
+        .substring(1)
+        .replace(/%22/g, '"')
+        .replace(/%7B/g, "{")
+        .replace(/%3A/g, ":")
+        .replace(/%2C/g, ",")
+        .replace(/%7D/g, "}")
+    );
 
-    const { arbitrableContractAddress, arbitratorContractAddress, disputeID } = message;
+    const {
+      arbitrableContractAddress,
+      arbitratorContractAddress,
+      disputeID,
+    } = message;
 
-    if (!arbitrableContractAddress || !disputeID || !arbitratorContractAddress) return;
+    if (!arbitrableContractAddress || !disputeID || !arbitratorContractAddress)
+      return;
 
     let question = {};
 
-    const realitioProxyContractInstance = new web3.eth.Contract(RealitioProxyContract.abi, arbitrableContractAddress);
+    const realitioProxyContractInstance = new web3.eth.Contract(
+      RealitioProxyContract.abi,
+      arbitrableContractAddress
+    );
 
-    const realitioContractAddress = await realitioProxyContractInstance.methods.realitio().call();
-    const realitioContractInstance = new web3.eth.Contract(RealitioContract.abi, realitioContractAddress);
+    const realitioContractAddress = await realitioProxyContractInstance.methods
+      .realitio()
+      .call();
+    const realitioContractInstance = new web3.eth.Contract(
+      RealitioContract.abi,
+      realitioContractAddress
+    );
 
     console.debug(`realitio ${realitioContractAddress}`);
 
-    const disputeIDToQuestionIDLogs = await realitioProxyContractInstance.getPastEvents("DisputeIDToQuestionID", {
-      filter: {
-        _disputeID: disputeID,
-      },
-      fromBlock: 0,
-      toBlock: "latest",
-    });
+    const disputeIDToQuestionIDLogs = await realitioProxyContractInstance.getPastEvents(
+      "DisputeIDToQuestionID",
+      {
+        filter: {
+          _disputeID: disputeID,
+        },
+        fromBlock: 0,
+        toBlock: "latest",
+      }
+    );
 
     if (disputeIDToQuestionIDLogs.length !== 1) return;
 
@@ -95,22 +72,28 @@ class RealitioDisplayInterface extends Component {
 
     console.debug(`questionID ${questionID}`);
 
-    const questionEventLog = await realitioContractInstance.getPastEvents("LogNewQuestion", {
-      filter: {
-        question_id: questionID,
-      },
-      fromBlock: 0,
-      toBlock: "latest",
-    });
+    const questionEventLog = await realitioContractInstance.getPastEvents(
+      "LogNewQuestion",
+      {
+        filter: {
+          question_id: questionID,
+        },
+        fromBlock: 0,
+        toBlock: "latest",
+      }
+    );
 
     const templateID = questionEventLog[0].returnValues.template_id;
-    const templateEventLog = await realitioContractInstance.getPastEvents("LogNewTemplate", {
-      filter: {
-        template_id: templateID,
-      },
-      fromBlock: 0,
-      toBlock: "latest",
-    });
+    const templateEventLog = await realitioContractInstance.getPastEvents(
+      "LogNewTemplate",
+      {
+        filter: {
+          template_id: templateID,
+        },
+        fromBlock: 0,
+        toBlock: "latest",
+      }
+    );
 
     console.debug(`templateID ${templateID}`);
     console.debug(templateEventLog);
@@ -118,7 +101,9 @@ class RealitioDisplayInterface extends Component {
     const template = JSON.parse(templateEventLog[0].returnValues.question_text);
     console.debug(template);
 
-    const questionText = questionEventLog[0].returnValues.question.split("\u241f");
+    const questionText = questionEventLog[0].returnValues.question.split(
+      "\u241f"
+    );
     question.text = questionText;
 
     this.setState({ question, template });
@@ -133,16 +118,45 @@ class RealitioDisplayInterface extends Component {
     }
 
     return (
-      <div>
-        <Header>
-          <SquareLeft />
-          <SquareRight />
-          <img src={RealitioLogo} />
-        </Header>
-        <QuestionText>{template.title}</QuestionText>
-        <QuestionLink href={`https://realitio.github.io/#!/question/${question.questionID}`} target="_blank" rel="noopener noreferrer">
-          Question Details
-        </QuestionLink>
+      <div
+        style={{
+          backgroundColor: "#f0f4f8",
+          padding: "16px",
+          fontFamily: "Roboto, sans-serif",
+        }}
+      >
+        <div>
+          <img src={RealityLogo} alt="Logo of reality.eth" />
+        </div>
+        <hr
+          style={{
+            height: "3px",
+            border: "none",
+            backgroundSize: "contain",
+            color: "#27b4ee",
+            background:
+              "linear-gradient(45deg, #24b3ec 0%, #24b3ec 93%, #b9f9fb  93%, #b9f9fb  95%, #dcfb6c 95%)",
+          }}
+        />
+        <div
+          style={{
+            marginTop: "16px",
+            marginBottom: "32px",
+            fontSize: "18x",
+            lineHeight: "1.4",
+            wordBreak: "break-word",
+          }}
+        >
+          {template.title}
+        </div>
+        <a
+          style={{ color: "#2093ff" }}
+          href={`https://realitio.github.io/#!/question/${question.questionID}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See on reality.eth
+        </a>
       </div>
     );
   }
